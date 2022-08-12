b8e87ff8fbf678e44fb1ad9f3a95b67ed029587d
Jonathan Thompson <jathompson399@gmail.com>
2022-08-11 20:34:18 -0500

    finish 4

--- server.js
+++ server.js
@@ -79,6 +79,18 @@ function validateAnimal(animal) {
     return true;
 }
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/animals', (req, res) => {
     let result = animals
     if (req.query) {
